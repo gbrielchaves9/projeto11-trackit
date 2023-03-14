bc2903696206d6971863ada29c5516d1be63211a
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-03-14 11:48:47 -0300

    teste 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import styled from "styled-components"
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { useState } from "react";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+       
+      </header>
+    </div>
+  );
+}
+
